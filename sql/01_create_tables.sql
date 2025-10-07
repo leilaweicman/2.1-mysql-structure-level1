911fe8abc0b2d12e568d8fe5d54ce2d0d5d879d0
leilaweicman <leilaweic@hotmail.com>
2025-10-07 11:17:32 +0200

    feat: create tables employee and sale

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -47,3 +47,27 @@ CREATE TABLE customer
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
+
+CREATE TABLE employee
+(
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name  VARCHAR(100) NOT NULL,
+    last_name   VARCHAR(100) NOT NULL,
+    job_title   VARCHAR(100)
+);
+
+CREATE TABLE sale
+(
+    sale_id     INT AUTO_INCREMENT PRIMARY KEY,
+    sale_date   DATE NOT NULL,
+    customer_id INT NOT NULL,
+    employee_id INT NOT NULL,
+    FOREIGN KEY (customer_id)
+        REFERENCES customer(customer_id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (employee_id)
+        REFERENCES employee(employee_id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
