0bbc908b8445a2720512365846aeb6f0c929d3e8
leilaweicman <leilaweic@hotmail.com>
2025-10-07 13:01:32 +0200

    feat: insert data into supplier table

---
+++ sql/02_insert_data.sql
@@ -0,0 +1,10 @@
+USE optics;
+
+INSERT INTO supplier (name, street, number, city, postal_code, country, phone, fax, tax_id)
+VALUES ('VisionPro Ltd.', 'High Street', '12', 'London', 'W1B 3HH', 'UK', '+44 203456789', '+44 203456790',
+        'GB1234567'),
+       ('OptiWorld GmbH', 'Marktplatz', '8', 'Munich', '80331', 'Germany', '+49 892345678', '+49 892345679',
+        'DE7654321'),
+       ('Lentix SA', 'Carrer de Mallorca', '240', 'Barcelona', '08008', 'Spain', '+34 932222333', '+34 932222334',
+        'ES9988776');
+
