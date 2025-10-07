49107ff2abe3147d8b83942ad6dfcf7d1d7d010d
leilaweicman <leilaweic@hotmail.com>
2025-10-07 13:04:16 +0200

    feat: insert data into glasses table

--- sql/02_insert_data.sql
+++ sql/02_insert_data.sql
@@ -8,3 +8,10 @@ VALUES ('VisionPro Ltd.', 'High Street', '12', 'London', 'W1B 3HH', 'UK', '+44 2
        ('Lentix SA', 'Carrer de Mallorca', '240', 'Barcelona', '08008', 'Spain', '+34 932222333', '+34 932222334',
         'ES9988776');
 
+INSERT INTO glasses (brand, right_lens_power, left_lens_power, frame_type, frame_color, right_lens_color,
+                     left_lens_color, price, supplier_id)
+VALUES ('Ray-Ban', 1.25, 1.50, 'plastic', 'black', 'clear', 'clear', 129.90, 1),
+       ('Oakley', -0.75, -0.75, 'metallic', 'silver', 'gray', 'gray', 179.50, 1),
+       ('Persol', 0.00, 0.00, 'floating', 'brown', 'clear', 'clear', 199.00, 2),
+       ('Vogue', -2.25, -2.00, 'plastic', 'red', 'clear', 'clear', 99.00, 3),
+       ('Prada', 0.75, 1.00, 'metallic', 'gold', 'brown', 'brown', 249.99, 3);
