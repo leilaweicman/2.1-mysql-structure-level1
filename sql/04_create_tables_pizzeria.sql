27c1e9b4c7626f7c0bd30617dd235afb7081122b
leilaweicman <leilaweic@hotmail.com>
2025-10-08 11:20:39 +0200

    feat(pizzeria): create tables employee and category

--- sql/04_create_tables_pizzeria.sql
+++ sql/04_create_tables_pizzeria.sql
@@ -18,21 +18,38 @@ CREATE TABLE locality
 CREATE TABLE customer
 (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name        VARCHAR(100) NOT NULL,
-    last_name        VARCHAR(100) NOT NULL,
-    address VARCHAR(150) NOT NULL,
+    first_name  VARCHAR(100) NOT NULL,
+    last_name   VARCHAR(100) NOT NULL,
+    address     VARCHAR(150) NOT NULL,
     postal_code VARCHAR(10),
-    phone VARCHAR(20) NOT NULL,
-    locality_id INT NOT NULL,
-    FOREIGN KEY (locality_id) REFERENCES locality(locality_id)
+    phone       VARCHAR(20)  NOT NULL,
+    locality_id INT          NOT NULL,
+    FOREIGN KEY (locality_id) REFERENCES locality (locality_id)
 );
 
 CREATE TABLE store
 (
-    store_id INT AUTO_INCREMENT PRIMARY KEY,
-    address VARCHAR(150) NOT NULL,
+    store_id    INT AUTO_INCREMENT PRIMARY KEY,
+    address     VARCHAR(150) NOT NULL,
     postal_code VARCHAR(10),
-    locality_id INT NOT NULL,
-    FOREIGN KEY (locality_id) REFERENCES locality(locality_id)
+    locality_id INT          NOT NULL,
+    FOREIGN KEY (locality_id) REFERENCES locality (locality_id)
 );
 
+CREATE TABLE employee
+(
+    employee_id   INT AUTO_INCREMENT PRIMARY KEY,
+    first_name    VARCHAR(100) NOT NULL,
+    last_name     VARCHAR(100) NOT NULL,
+    tax_id        VARCHAR(20)  NOT NULL UNIQUE,
+    phone         VARCHAR(20)  NOT NULL,
+    employee_role ENUM('cook', 'delivery') NOT NULL,
+    store_id      INT          NOT NULL,
+    FOREIGN KEY (locality_id) REFERENCES locality (locality_id)
+);
+
+CREATE TABLE category
+(
+    category_id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
