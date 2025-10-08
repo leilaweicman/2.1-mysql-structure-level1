d0a6e4d88fc9afce4ff0f5881702660e75c1430b
leilaweicman <leilaweic@hotmail.com>
2025-10-08 11:15:36 +0200

    feat(pizzeria): create tables customer and store

--- sql/04_create_tables_pizzeria.sql
+++ sql/04_create_tables_pizzeria.sql
@@ -14,3 +14,25 @@ CREATE TABLE locality
     province_id INT          NOT NULL,
     FOREIGN KEY (province_id) REFERENCES province (province_id)
 );
+
+CREATE TABLE customer
+(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name        VARCHAR(100) NOT NULL,
+    last_name        VARCHAR(100) NOT NULL,
+    address VARCHAR(150) NOT NULL,
+    postal_code VARCHAR(10),
+    phone VARCHAR(20) NOT NULL,
+    locality_id INT NOT NULL,
+    FOREIGN KEY (locality_id) REFERENCES locality(locality_id)
+);
+
+CREATE TABLE store
+(
+    store_id INT AUTO_INCREMENT PRIMARY KEY,
+    address VARCHAR(150) NOT NULL,
+    postal_code VARCHAR(10),
+    locality_id INT NOT NULL,
+    FOREIGN KEY (locality_id) REFERENCES locality(locality_id)
+);
+
